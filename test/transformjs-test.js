226e9fc277b8fdf082b37672b733632992fd4395
Joe Hewitt <joe@joehewitt.com>
2014-02-20 17:15:31 -0800

    * Update to UglifyJS2 (which is a major rewrite and forced a rewrite on me)

--- test/transformjs-test.js
+++ test/transformjs-test.js
@@ -2,9 +2,8 @@ var path = require('path'),
     assert = require('assert'),
     vows = require('vows');
 
-require.paths.unshift(path.join(__dirname, '..', 'lib'));
-
 var transformjs = require('transformjs');
+var uglify = require('uglify-js');
 
 // *************************************************************************************************
 
@@ -15,15 +14,16 @@ vows.describe('basics').addBatch({
 
             var ast = transformjs.transform(source, [
                 function(node, next) {
-                    if (node.type == 'num' && node.value > 5) {
-                        return {type: 'num', value: 5};
+                    if (node.TYPE == 'Number' && node.getValue() > 5) {
+                        return new uglify.AST_Number({value: 5});
+                    } else {
+                        return next();                        
                     }
-                    return next();
                 }
             ]);
 
             var output = transformjs.generate(ast);
-            assert.equal(output, 'var a=1,b=5');
+            assert.equal(output, 'var a=1,b=5;');
         },
 
         'replace number': function() {
@@ -31,15 +31,16 @@ vows.describe('basics').addBatch({
 
             var ast = transformjs.transform(source, [
                 function(node, next) {
-                    if (node.type == 'num' && node.value > 5) {
-                        return {type: 'num', value: 5};
+                    if (node.TYPE == 'Number' && node.getValue() > 5) {
+                        return new uglify.AST_Number({value: 5});
+                    } else {
+                        return next();                        
                     }
-                    return next();
                 }
             ]);
 
             var output = transformjs.generate(ast);
-            assert.equal(output, 'a=1+5');
+            assert.equal(output, 'a=1+5;');
         },
 
         'strips else': function() {
@@ -47,8 +48,8 @@ vows.describe('basics').addBatch({
 
             var ast = transformjs.transform(source, [
                 function(node, next) {
-                    if (node.type == 'if') {
-                        return next(node.ifBlock);
+                    if (node.TYPE == 'If') {
+                        return next(node.body);
                     }
                     return next();
                 }
@@ -63,19 +64,21 @@ vows.describe('basics').addBatch({
 
             var ast = transformjs.transform(source, [
                 function(node, next) {
-                    if (node.type == 'if') {
+                    if (node.TYPE == 'If') {
                         var cond = node.condition;
-                        if (cond.type == 'call') {
-                            if (cond.left.type == 'name' && cond.left.name == 'has') {
+                        if (cond.TYPE == 'Call') {
+                            if (cond.expression.TYPE == 'SymbolRef' && cond.expression.name == 'has') {
                                 var args = cond.args;
                                 if (args.length == 1) {
                                     var featureNode = args[0];
-                                    if (featureNode.type == 'string') {
+                                    if (featureNode.TYPE == 'String') {
                                         var feature = featureNode.value;
                                         if (feature == 'bar') {
-                                            return next(node.ifBlock);
+                                            return next(node.body);
+                                        } else if (node.alternative) {
+                                            return next(node.alternative);
                                         } else {
-                                            return next(node.elseBlock);
+                                            return next(new uglify.AST_Null());
                                         }
                                     }
                                 }
@@ -97,7 +100,7 @@ vows.describe('basics').addBatch({
                 transformjs.transform(source, []);
                 assert.fail();
             } catch (exc) {
-                assert.equal(exc.line, 0);
+                assert.equal(exc.line, 1);
                 assert.equal(exc.col, 4);
                 assert.equal(exc.message, 'Invalid syntax: 1a');
             }
@@ -115,18 +118,72 @@ vows.describe('basics').addBatch({
             assert.equal(output, 'switch(a){case b:99;break;case c:88;break;default:77}');
         },
 
-        'object': function() {
-            var source = "var a = {foo: function() {42}}";
+        'branch removal': function() {
+            function testSource(source, expected) {
+                var ast = transformjs.transform(source, [
+                    transformjs.branchRemover(function(node, next) {
+                        if (node.TYPE == 'Call') {
+                            if (node.expression.TYPE == 'SymbolRef'
+                                && node.expression.name == 'has') {
+                                var args = node.args;
+                                if (args.length == 1) {
+                                    var arg1 = args[0];
+                                    if (arg1.TYPE == 'String') {
+                                        var str = arg1.value;
+                                        return str == 'bullseye' ? 'pass' : 'fail';
+                                    }
+                                }
+                            }
+                        }
+                    })
+                ]);
+                var output = transformjs.generate(ast);
+                assert.equal(output, expected);
+            }
 
-            var ast = transformjs.transform(source, [
-                function(node, next) {
-                    console.log(require('util').inspect(node, null, 200));
-                    return next();
-                }
-            ]);
-            var output = transformjs.generate(ast);
-            assert.equal(output, 'try{a}catch(exc){b}finally{c}');
+            testSource("if (true) { 42 } else { 66 }", "{42}");
+            testSource("if (false) { 42 } else { 66 }", "{66}");
+            testSource("true ? 42 : 66", "42;");
+            testSource("has('bullseye') ? 1 : 2", "1;");
+            testSource("has('bullseye') && false ? 1 : 2", "2;");
+            testSource("has('bullseye') && 0 ? 1 : 2", "2;");
+            testSource("has('bull') ? 1 : 2", "2;");
+            testSource("has('bull') || true ? 1 : 2", "1;");
+            testSource("has('bull') || 1 ? 1 : 2", "1;");
+            testSource("has('bull') || 0 ? 1 : 2", "2;");
         },
+
+        'multiple filters': function() {
+            function testSource(source, expected) {
+                var ast = transformjs.transform(source, [
+                    function(node, next) {
+                        // D&&D('a');
+                        return next();
+                    },
+                    function(node, next) {
+                        // D&&D('b');
+                        return next();
+                    }
+                ]);
+                var output = transformjs.generate(ast);
+                assert.equal(output, expected);
+            }
+
+            testSource("a", "a;");
+        },
+
+        // 'object': function() {
+        //     var source = "var a = {foo: function() {42}}";
+
+        //     var ast = transformjs.transform(source, [
+        //         function(node, next) {
+        //             console.log(require('util').inspect(node, null, 200));
+        //             return next();
+        //         }
+        //     ]);
+        //     var output = transformjs.generate(ast);
+        //     assert.equal(output, 'try{a}catch(exc){b}finally{c}');
+        // },
 
         // 'stuff': function() {
         //     var source = "true";
